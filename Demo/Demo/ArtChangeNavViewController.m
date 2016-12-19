d43e7de5eb3d3fdfa5a92bc4f822dcc0d8f59efc
魏景云 <wei_jingyun@outlook.com>
2016-12-19 15:24:42 +0800

    Demo修改加说明

--- Demo/Demo/ArtChangeNavViewController.m
+++ Demo/Demo/ArtChangeNavViewController.m
@@ -10,11 +10,49 @@
 #import "JYPagingView.h"
 #import "ArtTableViewController.h"
 
+@interface ArtNavView : UIView
+
+@property (nonatomic, strong) UIButton *leftBut;
+
+@end
+
+@implementation ArtNavView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    
+    if (self = [super initWithFrame:frame]) {
+        [self configUI];
+    }
+    return self;
+}
+
+- (void) configUI {
+    /*
+     * 只是做一个简单示例，要加分割线或其它变化，自行扩展即可
+     */
+    self.backgroundColor = [UIColor colorWithWhite:1 alpha: 0];
+    UIButton *but = [UIButton buttonWithType:UIButtonTypeSystem];
+    but.frame = CGRectMake(0, 22, 44, 44);
+    UIImage *buttonimage = [UIImage imageNamed:@"barbuttonicon_back"];
+    [but setImage:buttonimage forState:UIControlStateNormal];
+    but.tintColor = [UIColor colorWithWhite:0 alpha: 1];
+    self.leftBut = but;
+    [self addSubview:but];
+}
+
+- (void)changeAlpha:(CGFloat)alpha {
+    
+    self.backgroundColor = [UIColor colorWithWhite:1 alpha: alpha];
+    self.leftBut.tintColor = [UIColor colorWithWhite:(1 - alpha) alpha:1];
+}
+
+@end
+
 @interface ArtChangeNavViewController ()<HHHorizontalPagingViewDelegate>
 
 @property (nonatomic, strong) HHHorizontalPagingView *pagingView;
 
-@property (nonatomic, strong) UIView *navView;
+@property (nonatomic, strong) ArtNavView *navView;
 
 
 @end
@@ -28,25 +66,31 @@
     self.view.backgroundColor = [UIColor colorWithRed:242./255. green:242./255. blue:242./255. alpha:1.0];
     [self.pagingView reload];
     
-    
-    self.navView = [[UIView alloc] init];
-    [self.view addSubview:self.navView];
-    self.navView.backgroundColor = [UIColor colorWithWhite:1 alpha: 0];
+    /* 需要设置self.edgesForExtendedLayout = UIRectEdgeNone; 最好自定义导航栏 
+     * 在代理 - (void)pagingView:(HHHorizontalPagingView *)pagingView scrollTopOffset:(CGFloat)offset
+     *做出对应处理来改变 背景色透明度
+     */
+    self.navView = [[ArtNavView alloc] init];
     CGSize size = [UIScreen mainScreen].bounds.size;
     self.navView.frame = CGRectMake(0, 0, size.width, 84);
-    
+    [self.view addSubview:self.navView];
+    [self.navView.leftBut addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (void)viewDidAppear:(BOOL)animated{
+- (void)back {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 }
 
-- (void)viewWillAppear:(BOOL)animated{
+- (void)viewWillAppear:(BOOL)animated {
     [self.navigationController setNavigationBarHidden:YES animated:animated];
     [super viewWillAppear:animated];
 }
 
-- (void)viewWillDisappear:(BOOL)animated{
+- (void)viewWillDisappear:(BOOL)animated {
     [self.navigationController setNavigationBarHidden:NO animated:animated];
     [super viewWillDisappear:animated];
 }
@@ -56,7 +100,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)showText:(NSString *)str{
+- (void)showText:(NSString *)str {
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:str delegate: self  cancelButtonTitle:nil otherButtonTitles:nil];
     [alert show];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
@@ -66,7 +110,7 @@
 
 #pragma mark -  HHHorizontalPagingViewDelegate
 // 下方左右滑UIScrollView设置
-- (NSInteger)numberOfSectionsInPagingView:(HHHorizontalPagingView *)pagingView{
+- (NSInteger)numberOfSectionsInPagingView:(HHHorizontalPagingView *)pagingView {
     return 5;
 }
 
@@ -79,11 +123,11 @@
 }
 
 //headerView 设置
-- (CGFloat)headerHeightInPagingView:(HHHorizontalPagingView *)pagingView{
+- (CGFloat)headerHeightInPagingView:(HHHorizontalPagingView *)pagingView {
     return 250;
 }
 
-- (UIView *)headerViewInPagingView:(HHHorizontalPagingView *)pagingView{
+- (UIView *)headerViewInPagingView:(HHHorizontalPagingView *)pagingView {
     
     UIView *headerView = [[UIView alloc] init];
     headerView.backgroundColor = [UIColor orangeColor];
@@ -101,11 +145,11 @@
 }
 
 //segmentButtons
-- (CGFloat)segmentHeightInPagingView:(HHHorizontalPagingView *)pagingView{
+- (CGFloat)segmentHeightInPagingView:(HHHorizontalPagingView *)pagingView {
     return 36.;
 }
 
-- (NSArray<UIButton*> *)segmentButtonsInPagingView:(HHHorizontalPagingView *)pagingView{
+- (NSArray<UIButton*> *)segmentButtonsInPagingView:(HHHorizontalPagingView *)pagingView {
     
     NSMutableArray *buttonArray = [NSMutableArray array];
     for(int i = 0; i < 6; i++) {
@@ -151,13 +195,12 @@
     float al = 1.0 - fz / fm;
     al = al <= 0.05 ? 0 : al;
     al = al >= 0.95 ? 1 : al;
-       NSLog(@"__ %f  __  %f __ %lf",offset,self.pagingView.pullOffset, al);
-    self.navView.backgroundColor = [UIColor colorWithWhite:1 alpha: al];
-    
+//       NSLog(@"__ %f  __  %f __ %lf",offset,self.pagingView.pullOffset, al);
+    [self.navView changeAlpha:al];
 }
 
 #pragma mark - 懒加载
-- (HHHorizontalPagingView *)pagingView{
+- (HHHorizontalPagingView *)pagingView {
     if (!_pagingView) {
         CGSize size = [UIScreen mainScreen].bounds.size;
         _pagingView = [[HHHorizontalPagingView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height) delegate:self];
